aa17dc3734677472fe9e0b89dced6c9161b52859
user <user@DESKTOP-GON99JR>
2023-03-19 12:40:59 +0800

    加入專案檔案。

---
+++ main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N, sum, i;
+	i = 1;
+	sum = 0;
+	cin >> N;
+	while (i <= N)
+	{
+		if (i % 3 == 0)
+		{
+			sum += i;
+		}
+		i++;
+	}
+	cout << sum << endl;
+	return 0;
+}
